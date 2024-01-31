99c7b9696fd400f20ffa13298de3f1d9799988d9
eduviereisrael <eduviereisrael@gmail.com>
2024-02-01 00:41:42 +0100

    checking styled components

---
+++ app/Components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NavbarDiv } from './navbar.style'
+
+const Navbar = () => {
+  return (
+    <NavbarDiv>This is the Navbar</NavbarDiv>
+  )
+}
+
+export default Navbar
---
+++ app/Components/Navbar/navbar.style.js
@@ -0,0 +1,8 @@
+"use client"
+
+import { styled } from "styled-components"
+
+export const NavbarDiv = styled.div`
+    width: 100%;
+    padding: 20px 80px;
+`
